49f0b16b3361cdd39296abacaa428be614c5d0e2
OddieOldDev <Lewiscollier12@outlook.com>
2023-09-22 22:29:04 +0100

    asset/blink

--- .bat
+++ .bat
@@ -1,5 +1,3 @@
-:: This batch file was designed by Fonalc to help publish changes, You may download this to help you.
-
 @echo off
 title Github Commiter 9000
 cd C:\Users\Lewis\Desktop\rat attak\Github Page\OddieOldDev.github.io
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OddieOldDev.github.io
+
+Mainly used for holding assets and info on my games.
+# OddieOldDev.github.io/MonkScare.html
